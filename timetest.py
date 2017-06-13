404a46e65ec30136ca216c0396e8400d3b2549e1
Darius <miu.jonathan@gmail.com>
2017-06-13 07:53:09 -0400

    testing time algorithm

--- timetest.py
+++ timetest.py
@@ -11,9 +11,9 @@ for i in range(100):
 	nextupdate = currenttime = time.time() + (3690 * i)
 	nextupdate = (nextupdate - (nextupdate % (delay * 60))) + (delay * 60)
 	#print(time.localtime(currenttime).tm_hour % 3)
-	if time.localtime(currenttime).tm_hour % 3 == 2 : 
-		nextupdate = nextupdate - 7200
-	else : nextupdate = nextupdate + 3600
+	#if time.localtime(currenttime).tm_hour % 3 == 2 : 
+	#	nextupdate = nextupdate - 7200
+	#else : nextupdate = nextupdate + 3600
 	
 	noowtime = 'current test time: '
 	if time.localtime(currenttime).tm_hour < 10 : noowtime = noowtime + '0'
